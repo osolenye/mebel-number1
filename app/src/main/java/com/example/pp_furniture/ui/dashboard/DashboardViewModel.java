66808dc3c0200140634a4d08644186de872163ed
osolenye <apasniynegr228@gmail.com>
2024-04-11 21:47:05 +0600

    main menu done

---
+++ app/src/main/java/com/example/pp_furniture/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,16 @@
+package com.example.pp_furniture.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+
+
+    public DashboardViewModel() {
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/pp_furniture/ui/home/HomeFragment.java
@@ -0,0 +1,79 @@
+package com.example.pp_furniture.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.example.pp_furniture.R;
+import com.example.pp_furniture.databinding.FragmentHomeBinding;
+import com.example.pp_furniture.models.CategoryModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeFragment extends Fragment {
+
+
+    private FragmentHomeBinding binding;
+    List<CategoryModel> list_category = new ArrayList<>();
+    CategoryAdapter adapter;
+    LottieAnimationView lotty_sale;
+    NavController navController;
+
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        lotty_sale = binding.lottySale;
+        lotty_sale.setAnimation(R.raw.furniture);
+
+
+        createList();
+        adapter = new CategoryAdapter();
+        adapter.setMain_list(list_category);
+        binding.rvCatalogCategory.setAdapter(adapter);
+
+        return root;
+    }
+
+    private void createList() {
+        list_category.add(new CategoryModel("Для спальни", R.drawable.bed_parlament));
+        list_category.add(new CategoryModel("Для гостинной",R.drawable.sofa_yellow));
+        list_category.add(new CategoryModel("Кухонная мебель", R.drawable.kuh_furn));
+        list_category.add(new CategoryModel("Юношеские гарнитуры",R.drawable.detskii_garnitur58));
+        list_category.add(new CategoryModel("Садовая мебель", R.drawable.garden_furn));
+        list_category.add(new CategoryModel("Мебель для прихожей",R.drawable.prihozhaya_mebel));
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.saleCardView.setOnClickListener(v->{
+            navController = Navigation.findNavController(requireActivity(),
+                    R.id.nav_host_fragment_activity_main);
+            navController.navigate(R.id.action_navigation_home_to_saleFragment);
+        });
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
